5744aa326e04733d04c36cd43c3b34fc122ecf19
Vedran Stankovic <vsrc@users.noreply.github.com>
2017-10-11 16:12:18 +0100

    tiedot

--- main.go
+++ main.go
@@ -1,103 +1,161 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/HouzuoGuo/tiedot/db"
+	"github.com/HouzuoGuo/tiedot/dberr"
 )
 
-var db *gorm.DB
-var err error
+/*
+In embedded usage, you are encouraged to use all public functions concurrently.
+However please do not use public functions in "data" package by yourself - you most likely will not need to use them directly.
 
-// Person struct serves as a model
-type Person struct {
-	ID        uint   `json:"id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	City      string `json:"city"`
-}
+To compile and run the example:
+    go build && ./tiedot -mode=example
+
+It may require as much as 1.5GB of free disk space in order to run the example.
+*/
 
 func main() {
+	// ****************** Collection Management ******************
 
-	// NOTE: See we're using = to assign the global var
-	// instead of := which would assign it only in this function
-	db, err = gorm.Open("sqlite3", "./gorm.db")
+	myDBDir := "./mydb"
+	os.RemoveAll(myDBDir)
+	defer os.RemoveAll(myDBDir)
 
+	// (Create if not exist) open a database
+	myDB, err := db.OpenDB(myDBDir)
 	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-
-	db.AutoMigrate(&Person{})
-
-	r := gin.Default()
-	r.GET("/people/", GetPeople)
-	r.GET("/people/:id", GetPerson)
-	r.POST("/people", CreatePerson)
-	r.PUT("/people/:id", UpdatePerson)
-	r.DELETE("/people/:id", DeletePerson)
-
-	r.Run(":8765")
-}
-
-// DeletePerson deletes a record by given id
-func DeletePerson(c *gin.Context) {
-	id := c.Params.ByName("id")
-	var person Person
-	d := db.Where("id = ?", id).Delete(&person)
-	fmt.Println(d)
-	c.JSON(200, gin.H{"id #" + id: "deleted"})
-}
-
-// UpdatePerson updates a record by given id
-func UpdatePerson(c *gin.Context) {
-
-	var person Person
-	id := c.Params.ByName("id")
-
-	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	}
-	c.BindJSON(&person)
-
-	db.Save(&person)
-	c.JSON(200, person)
-
-}
-
-// CreatePerson creates a record with given information
-func CreatePerson(c *gin.Context) {
-
-	var person Person
-	c.BindJSON(&person)
-
-	db.Create(&person)
-	c.JSON(200, person)
-}
-
-// GetPerson gets a record with given id
-func GetPerson(c *gin.Context) {
-	id := c.Params.ByName("id")
-	var person Person
-	if err := db.Where("id = ?", id).First(&person).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		c.JSON(200, person)
-	}
-}
-
-// GetPeople gets all records
-func GetPeople(c *gin.Context) {
-	var people []Person
-	if err := db.Find(&people).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		c.JSON(200, people)
+		panic(err)
 	}
 
+	// Create two collections: Feeds and Votes
+	if err := myDB.Create("Feeds"); err != nil {
+		panic(err)
+	}
+	if err := myDB.Create("Votes"); err != nil {
+		panic(err)
+	}
+
+	// What collections do I now have?
+	for _, name := range myDB.AllCols() {
+		fmt.Printf("I have a collection called %s\n", name)
+	}
+
+	// Rename collection "Votes" to "Points"
+	if err := myDB.Rename("Votes", "Points"); err != nil {
+		panic(err)
+	}
+
+	// Drop (delete) collection "Points"
+	if err := myDB.Drop("Points"); err != nil {
+		panic(err)
+	}
+
+	// Scrub (repair and compact) "Feeds"
+	if err := myDB.Scrub("Feeds"); err != nil {
+		panic(err)
+	}
+
+	// ****************** Document Management ******************
+
+	// Start using a collection (the reference is valid until DB schema changes or Scrub is carried out)
+	feeds := myDB.Use("Feeds")
+
+	// Insert document (afterwards the docID uniquely identifies the document and will never change)
+	docID, err := feeds.Insert(map[string]interface{}{
+		"name": "Go 1.2 is released",
+		"url":  "golang.org"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Read document
+	readBack, err := feeds.Read(docID)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Document", docID, "is", readBack)
+
+	// Update document
+	err = feeds.Update(docID, map[string]interface{}{
+		"name": "Go is very popular",
+		"url":  "google.com"})
+	if err != nil {
+		panic(err)
+	}
+
+	// Process all documents (note that document order is undetermined)
+	feeds.ForEachDoc(func(id int, docContent []byte) (willMoveOn bool) {
+		fmt.Println("Document", id, "is", string(docContent))
+		return true  // move on to the next document OR
+		return false // do not move on to the next document
+	})
+
+	// Delete document
+	if err := feeds.Delete(docID); err != nil {
+		panic(err)
+	}
+
+	// More complicated error handing - identify the error Type.
+	// In this example, the error code tells that the document no longer exists.
+	if err := feeds.Delete(docID); dberr.Type(err) == dberr.ErrorNoDoc {
+		fmt.Println("The document was already deleted")
+	}
+
+	// ****************** Index Management ******************
+	// Indexes assist in many types of queries
+	// Create index (path leads to document JSON attribute)
+	if err := feeds.Index([]string{"author", "name", "first_name"}); err != nil {
+		panic(err)
+	}
+	if err := feeds.Index([]string{"Title"}); err != nil {
+		panic(err)
+	}
+	if err := feeds.Index([]string{"Source"}); err != nil {
+		panic(err)
+	}
+
+	// What indexes do I have on collection A?
+	for _, path := range feeds.AllIndexes() {
+		fmt.Printf("I have an index on path %v\n", path)
+	}
+
+	// Remove index
+	if err := feeds.Unindex([]string{"author", "name", "first_name"}); err != nil {
+		panic(err)
+	}
+
+	// ****************** Queries ******************
+	// Prepare some documents for the query
+	feeds.Insert(map[string]interface{}{"Title": "New Go release", "Source": "golang.org", "Age": 3})
+	feeds.Insert(map[string]interface{}{"Title": "Kitkat is here", "Source": "google.com", "Age": 2})
+	feeds.Insert(map[string]interface{}{"Title": "Good Slackware", "Source": "slackware.com", "Age": 1})
+
+	var query interface{}
+	json.Unmarshal([]byte(`[{"eq": "New Go release", "in": ["Title"]}, {"eq": "slackware.com", "in": ["Source"]}]`), &query)
+
+	queryResult := make(map[int]struct{}) // query result (document IDs) goes into map keys
+
+	if err := db.EvalQuery(query, feeds, &queryResult); err != nil {
+		panic(err)
+	}
+
+	// Query result are document IDs
+	for id := range queryResult {
+		// To get query result document, simply read it
+		readBack, err := feeds.Read(id)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Query returned document %v\n", readBack)
+	}
+
+	// Gracefully close database
+	if err := myDB.Close(); err != nil {
+		panic(err)
+	}
 }
